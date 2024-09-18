9b098fc915f6133a6971159c070cc5a061bd0493
Emerson Lapa <emersonlapa@outlook.com>
2024-09-18 20:32:15 -0300

    Aula11 e Aula12

---
+++ aula11/js/script.js
@@ -0,0 +1,12 @@
+alert('Exercicio Aula 11');
+
+let num1 = prompt('Olá digite um número: ');
+let num2 = prompt('Digite outro número: ');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert(`o resultado é: ${num1 + num2}`);
+alert('o resultado é: ' + (num1 + num2)); // Teste meu de curiosidade
