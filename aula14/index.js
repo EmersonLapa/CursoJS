0506cb066ca89aefa620e4d06028ec59210047e0
Emerson Rodrigues Lapa <emelapa@uolinc.com>
2024-10-16 17:20:03 -0300

    aula14

---
+++ aula14/index.js
@@ -0,0 +1,20 @@
+//IEEE 754-2008
+let num1 = 10.252; // number 
+let num2 = 2.5; // number
+
+let num3 = 0.7;
+let num4 = 0.8;
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1));
+// console.log(Number.isNaN(temp));
+// let temp = 'Emerson' * '5';
+// console.log(temp);
+
+num3 = Number(num3.toFixed(2));
+console.log(num3);
+console.log(num4.toFixed(2));
+console.log(Number.isInteger(num3));
