f742c7e6c461e0dc26b545cc54c502a04c343218
Emerson Lapa <emersonlapa@outlook.com>
2024-10-20 14:02:16 -0300

    Aula15

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let numero = 9.547892;
+// let numTratado = Math.floor(numero); // Arredondar valor para baixo
+// let numTratado = Math.ceil(numero); // Arredondar valor para cima
+// let numTratado = Math.round(numero); // Arredondar valor para cima ou para baixo ex 9.5 => 10 ou 9.4 => 9
+// console.log(numTratado);
+// console.log(Math.max(1,2,3,4,5,6,7,8,9,10,100)); // Essa função retorna o maior numero em um array
+// console.log(Math.min(1,2,3,4,5,6,7,8,9,10,100)); // Essa função retorna o menor numero em um array
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // Essa função Math.random retonar numeros aleatorios, nesse caso entre 5 e 10 e estão sendo arredondados atraves da função Math.round
+//console.log(Math.pow(2, 10)); // Calculo de potenciação 
+//console.log(2 ** 10); // Calculo de potenciação de outra maneira
+//console.log(100 / 0); // Lembre-se que por mais que seja errado, o javaScript deixa você realizar divisoes por 0
