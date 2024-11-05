2697fb9d8cfc39a8c0c478c77c58a5d24e549314
Emerson Rodrigues Lapa <emelapa@uolinc.com>
2024-11-05 15:59:43 -0300

    aula16

---
+++ aula16/index.js
@@ -0,0 +1,29 @@
+//              0           1           2        3
+const alunos = ['Emerson', 'Rodrigues', 'Lapa', 'Paloma'];
+
+//console.log(typeof alunos);
+//console.log(alunos instanceof Array);
+//alunos.push('Corsa');
+//alunos.push('Gol');
+// console.log(alunos.slice(0, -2));
+// console.log(alunos[50]);
+// delete alunos[1]; // o Delete, apaga um valor no indice especificado e este indice fica vazio.
+// console.log(alunos[1]);
+// console.log(alunos);
+// const removido = alunos.pop(); // a função "pop", remove o ultimo item no array.
+// const primeiroRemovido = alunos.shift(); // a função "shift", remove o primeiro item no array.
+//alunos.pop(); // a função "pop", remove o ultimo item no array.
+//alunos.shift(); // a função "shift", remove o primeiro item no array.
+// console.log(primeiroRemovido);
+// console.log(removido);
+// alunos.unshift('Uno'); // a função "unshift", adiciona um item no começo de um array. 
+// alunos.unshift('Gol'); // a função "unshift", adiciona um item no começo de um array.
+// alunos.push('Corsa'); // a função "push", adiciona um item no fim de um array.
+// alunos.push('Chevete'); // a função "push", adiciona um item no fim de um array.
+// alunos[alunos.length] = 'Corsa';
+// alunos[alunos.length] = 'Gol';
+// alunos[alunos.length] = 'Uno';
+// alunos[0] = 'Paloma';
+// alunos[4] = 'De Paula';
+// console.log(alunos);
+// console.log(alunos[3]);
