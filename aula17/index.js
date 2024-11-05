99753a0486bcfe1a790e5bee8943ee07f83b1683
Emerson Lapa <emersonlapa@outlook.com>
2024-11-05 19:41:13 -0300

    aula17

---
+++ aula17/index.js
@@ -0,0 +1,35 @@
+// opção 1 (modo clássico de declarar funções)
+// function soma (x = 1, y = 1) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado = soma(4 ,2);
+// console.log(resultado);
+
+
+// opção 2 (função anonima declara a função em uma variável e nesse caso a função precisa terminar com ";")
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// opção 3 (função (aeroFunction) declarada em uma variável mas sem o nome "function" e nesse caso a função precisa terminar com ";")
+
+// const raiz = (n) => {
+//     return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// opção 4 (função (aeroFunction) declarada em uma variável mas sem o nome "function", usada para funções simples, com uma linha de codigo por exemplo)
+// const raiz = n => n ** 0.5
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
